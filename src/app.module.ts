eb2055d2004a3bc607a0da89648de26bc083ebd8
ahn0530 <rlans101023@gmail.com>
2025-01-08 17:01:00 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './typeorm.config';
 import { UsersModule } from './users/users.module';
 import { RecyclablesModule } from './recyclables/recyclables.module';
 import { HistoryModule } from './history/history.module';
 import { RewardsModule } from './rewards/rewards.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, RecyclablesModule, HistoryModule, RewardsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    RecyclablesModule,
+    HistoryModule,
+    RewardsModule,
+  ],
 })
 export class AppModule {}

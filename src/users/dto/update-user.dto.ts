5e571a81b9ae9bdfbe437b5ae76781609403459d
ahn0530 <rlans101023@gmail.com>
2025-02-18 16:07:42 +0900

    user update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -5,7 +5,11 @@ import { IsOptional, IsString } from 'class-validator';
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsOptional()
   @IsString()
-  password?: string; // 비밀번호 업데이트 시 bcrypt 적용
+  password?: string; 
+
+  @IsOptional()
+  @IsString()
+  name?: string; 
 
   @IsOptional()
   points?: number;
